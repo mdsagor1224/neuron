7fc49e2748ffcc6a06c1f7ab4a1a0338f970d60c
mdsagor <mdsagorahmed1224@gmail.com>
2023-07-16 09:38:56 +0600

    nc3

--- functions.php
+++ functions.php
@@ -73,3 +73,19 @@ function neuron_theme_supports(){
 
 }
 add_action('after_setup_theme','neuron_theme_supports');
+
+function neuron_theme_custom_post(){
+    register_post_type( 'slide',
+        array(
+            'labels' => array(
+                'name' => __('Slides'),
+                'singular_name' => __('Slide')
+            ),
+            'supports' => array('title','editor', 'custom-fields', 'thumbnail', 'page-attributes'),
+            'public' => false,
+            'show_ui' => true
+            )
+
+        );
+}
+add_action('init','neuron_theme_custom_post');
